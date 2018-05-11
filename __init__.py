fdcf3643ddb864da7b6bb450b4457e10d52ccd1b
zaherairout <zaher2995>
2018-05-12 00:34:21 +0300

    update

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from mycroft import MycroftSkill, intent_handler
 
 class ImageCaptionSkill(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        MycroftSkill.__init__("ImageCaptionSkill")
 
     @intent_handler(IntentBuilder().require('ImageCaption'))
     def handle_image_caption(self, message):
