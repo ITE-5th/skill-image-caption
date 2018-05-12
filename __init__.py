6c242f655ab464eb462142652e7793e0f25ed186
zaherairout <zaher2995>
2018-05-12 14:31:51 +0300

    restructure

--- __init__.py
+++ __init__.py
@@ -1,14 +1,9 @@
-import json
 import socket
 from os.path import join, dirname, exists
 
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
-
-from connection_helper import ConnectionHelper
-from order_message.close_message import CloseMessage
-from order_message.image_to_text_message import ImageToTextMessage
 
 try:
     from camera import Camera
@@ -103,3 +98,90 @@ def create_settings_meta():
     if not exists(meta_path):
         with open(meta_path, 'w') as fp:
             json.dump(meta, fp)
+
+
+# Connection Helper
+import json
+import pickle
+
+
+class ConnectionHelper:
+
+    @staticmethod
+    def send_json(socket, data):
+        try:
+            serialized = json.dumps(data)
+        except (TypeError, ValueError) as e:
+            raise Exception('You can only send JSON-serializable data')
+        # send the length of the serialized data first
+        socket.send('%d\n'.encode() % len(serialized))
+        # send the serialized data
+        socket.sendall(serialized.encode())
+
+    @staticmethod
+    def receive_json(socket):
+        view = ConnectionHelper.receive(socket).decode()
+        try:
+            deserialized = json.loads(view)
+        except (TypeError, ValueError) as e:
+            raise Exception('Data received was not in JSON format')
+        return deserialized
+
+    @staticmethod
+    def send_pickle(socket, object):
+        try:
+            serialized = pickle.dumps(object)
+        except (TypeError, ValueError) as e:
+            raise Exception('You can only send JSON-serializable data')
+        # send the length of the serialized data first
+        socket.send('%d\n'.encode() % len(serialized))
+        # send the serialized data
+        socket.sendall(serialized)
+
+    @staticmethod
+    def receive_pickle(socket):
+        view = ConnectionHelper.receive(socket)
+        try:
+            deserialized = pickle.loads(view)
+        except (TypeError, ValueError) as e:
+            raise Exception('Data received was not in JSON format')
+        return deserialized
+
+    @staticmethod
+    def receive(socket):
+        # read the length of the data, letter by letter until we reach EOL
+        length_str = ''
+        char = socket.recv(1).decode()
+        if char == '':
+            return char
+        while char != '\n':
+            length_str += char
+            char = socket.recv(1).decode()
+        total = int(length_str)
+        # use a memoryview to receive the data chunk by chunk efficiently
+        view = memoryview(bytearray(total))
+        next_offset = 0
+        while total - next_offset > 0:
+            recv_size = socket.recv_into(view[next_offset:], total - next_offset)
+            next_offset += recv_size
+        view = view.tobytes()
+        return view
+
+
+# Message Classes
+
+class Message:
+    pass
+
+
+class CloseMessage(Message):
+    pass
+
+
+class ImageMessage(Message):
+    def __init__(self, image):
+        self.image = image
+
+
+class ImageToTextMessage(ImageMessage):
+    pass
--- order_message/image_message.py
+++
@@ -1,6 +0,0 @@
-from server.message.message import Message
-
-
-class ImageMessage(Message):
-    def __init__(self, image):
-        self.image = image
