5bc6e21d076cf0ae5aee01d5ef1b4536786aec9f
zaherairout <zaher2995>
2018-05-12 14:47:23 +0300

    restructure

--- __init__.py
+++ __init__.py
@@ -23,6 +23,8 @@ class ImageCaptionSkill(MycroftSkill):
     def __init__(self):
         super(ImageCaptionSkill, self).__init__("ImageCaptionSkill")
         # MycroftSkill.__init__("ImageCaptionSkill")
+        LOG.warning('Running Skill Image Captioning ')
+
         if "model" not in self.settings:
             self.settings["model"] = "vgg"
         if "server_url" not in self.settings:
@@ -40,18 +42,20 @@ class ImageCaptionSkill(MycroftSkill):
         self.socket.connect((self.host, self.port))
         LOG.info('connected to server:' + self.host + ' : ' + {str(self.port)})
 
-    @intent_handler(IntentBuilder("correct_name").require('ImageCaption'))
+    @intent_handler(IntentBuilder("CaptionIntent").require('ImageCaption'))
     def handle_image_caption(self, message):
+        LOG.info('Handling ' + message)
 
         image, _ = self.camera.take_image()
         if image is None:
-            return
+            return False
 
         order_message = ImageToTextMessage(image)
         ConnectionHelper.send_pickle(self.socket, order_message)
         response = ConnectionHelper.receive_json(self.socket)
         print(message)
         self.speak_dialog('image.caption', response)
+        return True
 
     def shutdown(self):
         super(ImageCaptionSkill, self).shutdown()
