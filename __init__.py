f3ffd996662ea39ec693c0f3a13842bcafaabfde
zaherairout <zaher2995>
2018-05-12 17:13:18 +0300

    restructure

--- __init__.py
+++ __init__.py
@@ -68,8 +68,6 @@ class ImageCaptionSkill(MycroftSkill):
         print('closing socket')
         self.socket.close()
 
-        self.camera.close()
-
 
 def create_skill():
     return ImageCaptionSkill()
@@ -206,18 +204,13 @@ import time
 
 LOG.warning('Running Skill Image Captioning 4')
 
-pi_camera = picamera.PiCamera()
-
-LOG.warning('Running Skill Image Captioning 5')
-
 
 class Camera:
 
     def __init__(self, width=800, height=600, vflip=True, hflip=True):
-        self.camera = pi_camera
-        self.camera.vflip = vflip
-        self.camera.hflip = hflip
-        self.camera.resolution = (width, height)
+        self.vflip = vflip
+        self.hflip = hflip
+        self.resolution = (width, height)
 
     def take_image(self, face_count=0):
         import os
@@ -227,7 +220,11 @@ class Camera:
 
         file_name = temp_dir + time.strftime("%Y%m%d-%H%M%S") + '.jpg'
 
-        self.camera.capture(file_name)
+        with picamera.PiCamera() as camera:
+            camera.resolution = self.resolution
+            camera.capture(file_name)
+            camera.close()
+
         with open(file_name, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
         if face_count > 0:
@@ -246,6 +243,3 @@ class Camera:
         has_one_face = len(rects) == faces_count
         print(has_one_face)
         return has_one_face
-
-    def close(self):
-        self.camera.close()
