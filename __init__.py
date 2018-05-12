17930d3576ee130f7ce110c1bd969b0c3bd12d20
zaherairout <zaher2995>
2018-05-12 15:20:54 +0300

    restructure

--- __init__.py
+++ __init__.py
@@ -17,6 +17,7 @@ except ImportError:
     msm.reload_skill("skill-image-caption")
 
 IMAGE_CAPTIONING_PORT = 9001
+LOG.warning('Running Skill Image Captioning ')
 
 
 class ImageCaptionSkill(MycroftSkill):
@@ -189,3 +190,53 @@ class ImageMessage(Message):
 
 class ImageToTextMessage(ImageMessage):
     pass
+
+
+# Camera
+
+import base64
+import time
+
+import picamera
+
+pi_camera = picamera.PiCamera()
+
+
+class Camera:
+
+    def __init__(self, width=800, height=600, vflip=True, hflip=True):
+        self.camera = pi_camera
+        self.camera.vflip = vflip
+        self.camera.hflip = hflip
+        self.camera.resolution = (width, height)
+
+    def take_image(self, face_count=0):
+        import os
+        temp_dir = './temp/'
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
+
+        file_name = temp_dir + time.strftime("%Y%m%d-%H%M%S") + '.jpg'
+
+        self.camera.capture(file_name)
+        with open(file_name, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+        if face_count > 0:
+            return encoded_string, file_name if self.check_faces(file_name=file_name, faces_count=face_count) else -1
+        # with open("../Image.jpg", "rb") as image_file:
+        #     encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+        return encoded_string, file_name
+
+    def check_faces(self, file_name='./temp/Image.jpg', faces_count=1):
+        import dlib
+        from skimage import io
+        print('analysing faces count')
+        detector = dlib.get_frontal_face_detector()
+        image = io.imread(file_name)
+        rects = detector(image, 1)
+        has_one_face = len(rects) == faces_count
+        print(has_one_face)
+        return has_one_face
+
+    def close(self):
+        self.camera.close()
