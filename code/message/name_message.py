1d02c3b9fa2ba48f284688d0fe05b03325bc9b6b
ZaherAirout <m.zaher.airout@gmail.com>
2018-07-03 10:18:02 +0300

    update messages

--- code/message/name_message.py
+++ code/message/name_message.py
@@ -2,5 +2,6 @@ from .message import Message
 
 
 class NameMessage(Message):
-    def __init__(self, name):
+    def __init__(self, name, user_name=None):
+        super().__init__(user_name)
         self.name = name.lower().replace(" ", "_")
