9048417df3fc5651b97c420f1c83f3a43b8a832f
ZaherAirout <m.zaher.airout@gmail.com>
2018-07-04 07:34:16 +0300

    update messages

--- code/misc/camera.py
+++ code/misc/camera.py
@@ -3,6 +3,8 @@ import time
 
 import picamera
 
+from .file_path_manager import FilePathManager
+
 
 class Camera:
 
@@ -14,6 +16,8 @@ class Camera:
     def take_image(self, face_count=0):
         import os
 
+        camera_file = FilePathManager.resolve('/resources/camera.wav')
+        os.system('aplay -Dhw:0,0 ' + camera_file)
         temp_dir = './temp/'
         if not os.path.exists(temp_dir):
             os.makedirs(temp_dir)
@@ -27,25 +31,27 @@ class Camera:
             camera.capture(file_name)
         with open(file_name, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-        if face_count > 0:
-            return encoded_string, file_name if self.check_faces(file_name=file_name, faces_count=face_count) else -1
+            if face_count > 0:
+                return (encoded_string, file_name) if self.check_faces(image_file=image_file,
+                                                                       faces_count=face_count) else (None, None)
         # with open("../Image.jpg", "rb") as image_file:
         #     encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
         return encoded_string, file_name
 
-    def check_faces(self, file_name='./temp/Image.jpg', faces_count=1):
+    def check_faces(self, image_file=None, faces_count=1):
         import dlib
-        from skimage import io
+        import numpy
+        from PIL import Image
         print('analysing faces count')
         detector = dlib.get_frontal_face_detector()
-        image = io.imread(file_name)
+        # from skimage import io
+        # image = io.imread(file_name)
+        image = numpy.asarray(Image.open(image_file))
+        image.setflags(write=True)
         rects = detector(image, 1)
         has_one_face = len(rects) == faces_count
         print(has_one_face)
         return has_one_face
-
-    def close(self):
-        self.camera.close()
 
 
 if __name__ == '__main__':
